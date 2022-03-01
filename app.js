23b290d37d31c964500a381e3fe5dd200bf8af38
Dylan-MDG <81700508+Dylan-MDG@users.noreply.github.com>
2022-03-01 16:50:03 -0500

    Adjusted tooltip pop-up
    Included Store #, Division, City and State, and adjusted hours of operations.

--- app.js
+++ app.js
@@ -44,29 +44,18 @@ function response(e) {
 };
 
     var description = `<img id="image"/>` +
-        `<h4><b>Hours of Operation: </b></h4>` +
-        `<p id='small-p'>S:&nbsp;` +
+        `<h4><b>Store #:&nbsp;` +
+        currentFeature.properties["RETAIL_STORE_D1_SK"] + `</br>` +
+        currentFeature.properties["CITY_NM"] + `,&nbsp;` +
+        currentFeature.properties["STATE_CD"] + `</b></h4>` +
+        `<p id='small-p'><b>Division:&nbsp;</b>` +
+        currentFeature.properties["DIVISION_NM"] +
+        `</br><b>Hours of Operation:&nbsp;</b>` +
         currentFeature.properties["SUNDAY_OPEN_HR"] + `-` +
         currentFeature.properties["SUNDAY_CLOSE_HR"] +
-        `</br>M:&nbsp;` +
-        currentFeature.properties["MONDAY_OPEN_HR"] + `-` +
-        currentFeature.properties["MONDAY_CLOSE_HR"] +
-        `</br>T:&nbsp;` +
-        currentFeature.properties["TUESDAY_OPEN_HR"] + `-` +
-        currentFeature.properties["TUESDAY_CLOSE_HR"] +
-        `</br>W:&nbsp;` +
-        currentFeature.properties["WEDNESDAY_OPEN_HR"] + `-` +
-        currentFeature.properties["WEDNESDAY_CLOSE_HR"] +
-        `</br>T:&nbsp;` +
-        currentFeature.properties["THURSDAY_OPEN_HR"] + `-` +
-        currentFeature.properties["THURSDAY_CLOSE_HR"] +
-        `</br>F:&nbsp;` +
-        currentFeature.properties["FRIDAY_OPEN_HR"] + `-` +
-        currentFeature.properties["FRIDAY_CLOSE_HR"] +
-        `</br>S:&nbsp;` +
-        currentFeature.properties["SATURDAY_OPEN_HR"] + `-` +
-        currentFeature.properties["SATURDAY_CLOSE_HR"] + `</p>`;
+        `</br></p>`;
 
+        
     const popups = document.getElementsByClassName("mapboxgl-popup");
     /** Check if there is already a popup on the map and if so, remove it */
     if (popups[0]) popups[0].remove();
